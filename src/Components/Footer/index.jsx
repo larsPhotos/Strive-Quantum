c0d64619112ce4de34c70d4fba0fb09d680e2631
Jesse Jenks <JesseEndoJenks@gmail.com>
2018-12-27 18:59:43 -0800

    update footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './style.css';
 
 export default class Footer extends Component {
   render() {
     return (
-    	<footer>&copy;{new Date().getFullYear()}</footer>
+    	<Navbar color="dark" expand="md" className="copyright">
+			<NavbarBrand>&copy;{new Date().getFullYear()}</NavbarBrand>
+    	</Navbar>
     );
   }
 }
